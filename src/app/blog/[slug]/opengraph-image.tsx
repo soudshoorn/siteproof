7765b1b12681e05083c42bc356a986c615c1af45
soudshoorn <sennaoudshoorn@gmail.com>
2026-02-17 11:54:57 +0100

    initial

--- src/app/blog/[slug]/opengraph-image.tsx
+++ src/app/blog/[slug]/opengraph-image.tsx
@@ -1,7 +1,6 @@
 import { ImageResponse } from "next/og";
 import { prisma } from "@/lib/db";
 
-export const runtime = "edge";
 export const alt = "SiteProof Blog";
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
